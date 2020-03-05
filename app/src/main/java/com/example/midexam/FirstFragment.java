d65f330aef71f4c8e36cf2c9d966cb8ea415ba1f
Arshpreet33 <arshpreetsingh33@gmail.com>
2020-03-05 15:03:23 -0500

    Second commit, basic functionalities implemented

---
+++ app/src/main/java/com/example/midexam/FirstFragment.java
@@ -0,0 +1,87 @@
+package com.example.midexam;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class FirstFragment extends Fragment {
+
+    Button btn_go;
+    private NavController navController;
+    ArrayList<Weather_> parray;
+    RecycleAdapter adapter;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        DataServices service = RetrofitClientInstance.getRetrofitInstance().create(DataServices.class);
+
+
+        Call<Weather> call =service.getCompleteWeather();
+        call.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                Weather weather = response.body();
+
+                try {
+                    parray = new ArrayList<>(weather.getWeather());
+                    generateView(parray,view);
+                }
+                catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void generateView(ArrayList<Weather_> pary, View view){
+
+        adapter = new RecycleAdapter(pary,getActivity().getApplicationContext());
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_view);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+}
